ac31e225ac43734768f15f72bc838c808112eb6a
hxzbaka <165454150+hxzbaka@users.noreply.github.com>
2024-07-30 15:09:35 +0800

    Fix USB connection
    Increase SELinux config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,3 +100,12 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# Mass storage
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.gs6/lun.0/file
+
+# SELinux
+TWHAVE_SELINUX := true
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+BOARD_SEPOLICY_UNION += unlabeled.te
+
