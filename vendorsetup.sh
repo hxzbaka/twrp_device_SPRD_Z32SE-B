6e600be9a5d668eeaf1df2c17d45bb79fdd43423
hxzbaka <165454150+hxzbaka@users.noreply.github.com>
2024-10-15 22:16:07 +0800

    Fix USB connection

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+patch -Np1 < device/sprd/LD_C13_TFT64/init.c.patch
 add_lunch_combo omni_LD_C13_TFT64-user
 add_lunch_combo omni_LD_C13_TFT64-userdebug
 add_lunch_combo omni_LD_C13_TFT64-eng
